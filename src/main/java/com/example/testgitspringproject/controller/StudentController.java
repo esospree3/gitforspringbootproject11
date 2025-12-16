c33a217d5804d6e2a8f78070efd5b6e35c99528e
esosp <esospree333@gmail.com>
2025-12-16 05:52:52 -0500

    a new branch with name feature/first-branch is created now

--- src/main/java/com/example/testgitspringproject/controller/StudentController.java
+++ src/main/java/com/example/testgitspringproject/controller/StudentController.java
@@ -9,7 +9,7 @@ public class StudentController {
 
     @GetMapping("/welcome")
     public String getMessage() {
-        System.out.println("from getMessage() of StudentController class");
+        System.out.println("from getMessage() of StudentController java class");
         return "Welcome to git from spring boot";
     }
 }

7005adcbcd2663165500bb54b72873d2d3020cf2
esosp <esospree333@gmail.com>
2025-12-16 05:18:54 -0500

    store changes for first time to git repo - https://github.com/esospree3/gitforspringbootproject11.git

---
+++ src/main/java/com/example/testgitspringproject/TestgitspringprojectApplication.java
@@ -0,0 +1,15 @@
+package com.example.testgitspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgitspringprojectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		SpringApplication.run(TestgitspringprojectApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
